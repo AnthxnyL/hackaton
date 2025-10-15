7dc4831e5565102f219dd4654d15014fad1f2a98
Leo-Riche <leo.riche@edu.devinci.fr>
2025-10-15 10:32:32 +0200

    feat: implement commentaries controller and model for CRUD operations

---
+++ back/models/commentaries.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import User from './usersModel.js';
+
+const commentariesSchema = new mongoose.Schema({
+    description: String,
+    createdAt: { type: Date, default: Date.now },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+});
+
+const Commentaries = mongoose.model('Commentaries', commentariesSchema);
+
+export default Commentaries;
